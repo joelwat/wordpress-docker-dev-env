d1f5cd2808dc9351e74adaeeb48944d406fccc58
Joel Watson <joelwat@gmail.com>
2021-02-21 20:34:12 -0800

    Gives db user ALL permission to wordpress db

--- docker/percona/init-db/2.create_user.sql
+++ docker/percona/init-db/2.create_user.sql
@@ -1,3 +1,3 @@
 CREATE USER 'wordpress-user'@'%' IDENTIFIED WITH mysql_native_password BY SECRET_PASSWORD_HERE; -- Replace with a real password.
-GRANT CREATE, SELECT, INSERT, UPDATE, DELETE ON wordpress.* TO 'wordpress-user'@'%';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress-user'@'%';
 FLUSH PRIVILEGES;
